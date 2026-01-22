bbd843802f25ab657b40dad823502bef98baf857
Ausni <ausnikafandob@gmail.com>
2026-01-22 12:03:47 +0100

    le script

--- tests/loginpom.spec.ts
+++ tests/loginpom.spec.ts
@@ -16,7 +16,7 @@ test.afterEach(async ({ page }) => {
 });
 
 
-test("login valid", { tag: "@regression" }, async ({ page }) => {
+test("login valid", { tag: "@valid" }, async ({ page }) => {
     await loginPage.setUsername("standard_user");
     await loginPage.setPassword("secret_sauce");
     await loginPage.loginBtn();
